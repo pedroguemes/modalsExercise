08d52848cc172ca0f0a9054c04133decdbc79abc
Pedro G <pedro9uemes@gmail.com>
2022-03-28 14:49:54 -0300

    events

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
+
+const closeModal = function(){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
 
 for (let i = 0; i < btnsOpenModal.length; i++){
   btnsOpenModal[i].addEventListener('click', function(){
@@ -12,8 +17,7 @@ for (let i = 0; i < btnsOpenModal.length; i++){
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
     })};
+
     
-    btnCloseModal.addEventListener('click', function(){
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
-  });
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
